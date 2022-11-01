16dd5b4216a6303705858e1ddd2878ea65a2ab88
Spyrosgg <spyrosgg@protonmail.com>
2022-11-01 17:19:47 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    // new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "StravaVisualizer",
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
+};
